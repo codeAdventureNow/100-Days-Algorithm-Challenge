775f45e8be0bc53471659e4e29f2c138e925acbc
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-05-20 15:53:18 -0700

    add two digits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,15 @@
-export function addTwoDigits(n: any): number {
+// my answer
 
+// export function addTwoDigits(n: any): number {
+//   let numberSplit = String(n).split('');
+//   return Number(numberSplit[0]) + Number(numberSplit[1]);
+// }
+
+//Dylan answer
+
+export function addTwoDigits(n: any): number {
+  let nums = n.toString().split('');
+  return parseInt(nums[0]) + parseInt(nums[1]);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(34));
