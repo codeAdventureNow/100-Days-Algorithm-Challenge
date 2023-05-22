01a60f58736aa90805dc0fecf3840dae0885eff9
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-05-22 07:56:19 -0700

    longest String

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,17 +1,17 @@
 // not sure if this right answer it is failing the test
 
-export function addBorder(picture: string[]): string[] {
-  const wall = '*'.repeat(picture[0].length + 2);
+// export function addBorder(picture: string[]): string[] {
+//   const wall = '*'.repeat(picture[0].length + 2);
 
-  picture.unshift(wall);
-  picture.push(wall);
+//   picture.unshift(wall);
+//   picture.push(wall);
 
-  for (let i = 1; i < picture.length - 1; i++) {
-    picture[i] = '*'.concat(picture[i], '*');
-  }
+//   for (let i = 1; i < picture.length - 1; i++) {
+//     picture[i] = '*'.concat(picture[i], '*');
+//   }
 
-  return picture;
-}
+//   return picture;
+// }
 
 export function addBorder(picture: string[]): string[] {
   const wall = '*'.repeat(picture[0].length + 2);
