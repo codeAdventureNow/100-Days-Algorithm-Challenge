a01f67a8ab2eca5a35c37b20fec70356e0ee6038
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-05-24 08:01:38 -0700

    alphabetic shift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,38 @@
 export function alphabeticShift(inputString: string): string {
+  const alphabet: object = {
+    a: 'b',
+    c: 'd',
+    d: 'e',
+    e: 'f',
+    f: 'g',
+    g: 'h',
+    h: 'i',
+    i: 'j',
+    j: 'h',
+    k: 'l',
+    m: 'n',
+    n: 'o',
+    o: 'p',
+    p: 'q',
+    q: 'r',
+    r: 's',
+    s: 't',
+    t: 'u',
+    u: 'v',
+    v: 'w',
+    x: 'y',
+    y: 'z',
+    z: 'a',
+  };
 
+  let inputShifted = inputString.split('');
+
+  for (let i = 0; i < inputShifted.length; i++) {
+    console.log(inputShifted[i]);
+    inputShifted[i] = alphabet[inputShifted[i]];
+  }
+
+  return inputShifted.join('');
 }
 
 console.log(alphabeticShift('crazy'));
