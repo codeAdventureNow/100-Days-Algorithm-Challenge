1c7d94c39055a44e390f3a0ea049d1eec56b7635
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-05-26 08:05:32 -0700

    alternating sums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,18 @@
 export function alternatingSums(a: number[]): number[] {
+  let evenSum = 0;
+  let oddSum = 0;
 
+  a.forEach((element, index) => {
+    const isEven = index % 2 === 0;
+
+    if (isEven) {
+      evenSum += element;
+    } else {
+      oddSum += element;
+    }
+  });
+
+  return [evenSum, oddSum];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
