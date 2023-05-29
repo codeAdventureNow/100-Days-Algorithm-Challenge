bb0831523ecdf3538ccfb8b1181f400901e94e70
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-05-29 09:21:26 -0700

    array change

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,6 +1,35 @@
+// export function areSimilar(a: number[], b: number[]): boolean {
+//   const c: number[] = [];
+//   let d: number[] = [];
+//   if (a.toString() === b.toString()) {
+//     return true;
+//   }
+
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] !== b[i]) {
+//       c.push(a[i]);
+//       d.push(b[i]);
+//     }
+//   }
+
+//   d = d.reverse();
+//   if (c.length === 2 && c.toString() === d.toString()) {
+//     return true;
+//   }
+
+//   return false;
+// }
+
+// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+
+//Why are we reversing d?
+
 export function areSimilar(a: number[], b: number[]): boolean {
   const c: number[] = [];
   let d: number[] = [];
+
   if (a.toString() === b.toString()) {
     return true;
   }
@@ -13,6 +42,7 @@ export function areSimilar(a: number[], b: number[]): boolean {
   }
 
   d = d.reverse();
+
   if (c.length === 2 && c.toString() === d.toString()) {
     return true;
   }
@@ -20,8 +50,7 @@ export function areSimilar(a: number[], b: number[]): boolean {
   return false;
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([3, 2, 3], [2, 3, 3]));
 
-//Why are we reversing d?
+//by reversing d we can see if the two numbers that did not match up a their index positions
+//if reversed are the same numbers,   we set c.length === 2 because are only allowing a swap one time(within one swap it takes two numbers)
