59063772d0f58cdbcd45b6d44705e41c2cf46603
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-06-02 08:18:22 -0700

    arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,37 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+// my solution
 
+// export function arrayReplace(
+//   inputArray: number[],
+//   elemToReplace: number,
+//   substitutionElem: number
+// ): number[] {
+//   const newArray: number[] = [];
+
+//   for (let i = 0; i < inputArray.length; i++) {
+//     if (inputArray[i] === elemToReplace) {
+//       inputArray[i] = substitutionElem;
+//       newArray.push(inputArray[i]);
+//     } else {
+//       newArray.push(inputArray[i]);
+//     }
+//   }
+
+//   return newArray;
+// }
+
+//his solution
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  inputArray.forEach((element, index) => {
+    if (element === elemToReplace) {
+      inputArray[index] = substitutionElem;
+    }
+  });
+  return inputArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([5, 2, 5, 4], 5, 7));
