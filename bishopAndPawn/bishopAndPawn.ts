7d1d38591f00efe412a04e0f5def743c81713668
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-06-04 08:37:18 -0700

    bishop and pawn

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,37 @@
 export function bishopAndPawn(bishop: string, pawn: string): boolean {
+  const board = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+  };
 
+  //for x we turned the letters into numbers
+  const bishopX = board[bishop[0]];
+  //for y the string number was parsed into an integer
+  const bishopY = parseInt(bishop[1]);
+  const pawnX = board[pawn[0]];
+  const pawnY = parseInt(pawn[1]);
+
+  //First Test
+  console.log(bishopX + bishopY);
+  console.log(pawnX + pawnY);
+
+  console.log(bishopX + pawnY);
+  console.log(bishopY + pawnX);
+
+  if (
+    bishopX + bishopY === pawnY + pawnX ||
+    bishopX + pawnY === pawnX + bishopY
+  ) {
+    return true;
+  }
+  return false;
 }
 
-// console.log(bishopAndPawn('a1', 'c3'));
+//    bishop x/y total 2
+console.log(bishopAndPawn('a1', 'c3'));
