ff6ac7f01776c149da5453cc929ff9e75bfaab0c
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-06-06 07:58:39 -0700

    candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,11 @@
 export function candies(n: number, m: number): number {
+  const candy: number = Math.floor(m / n);
 
+  return candy * n;
 }
 
-// console.log(candies(3, 10));
+// n-children, pieces of candy
+console.log(candies(3, 10));
+// console.log(candies(4, 10));
+// console.log(candies(4, 11));
+// console.log(candies(5, 11));
