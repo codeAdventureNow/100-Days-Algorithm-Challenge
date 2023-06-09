95fe3d24d179822b9fac4f97c84932ff6796a613
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-06-09 07:52:04 -0700

    characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,26 @@
+// export function characterParity(symbol: string): string {
+//   const isEven = parseInt(symbol) % 2 === 0;
+//   const notADigit = isNaN(parseInt(symbol));
+
+//   if (notADigit) {
+//     return 'not a digit';
+//   }
+
+//   return isEven ? 'even' : 'odd';
+// }
+
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
+console.log(characterParity('h'));
+console.log(characterParity('6'));
+console.log(characterParity('7'));
+
+//Double ternary
+
+//Dylans way
 export function characterParity(symbol: string): string {
+  const result = parseInt(symbol);
 
+  return isNaN(result) ? 'not a digit' : result % 2 === 0 ? 'even' : 'odd';
 }
-
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
