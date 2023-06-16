711790cf3eaba1858b591506e07528e0a31fbab0
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-06-16 09:47:08 -0700

    compare integers

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,31 @@
-export function compareIntegers(a: string, b: string): string {
+//my solution
+// export function compareIntegers(a: string, b: string): string {
+//   if (parseInt(a) === parseInt(b)) {
+//     return 'equals';
+//   }
+//   if (parseInt(a) < parseInt(b)) {
+//     return 'less';
+//   }
+//   if (parseInt(a) > parseInt(b)) {
+//     return 'greater';
+//   }
+// }
 
+//Dylan solution
+
+export function compareIntegers(a: string, b: string): string {
+  return parseInt(a) > parseInt(b)
+    ? 'greater'
+    : parseInt(a) < parseInt(b)
+    ? 'less'
+    : 'equal';
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));
+
+//parse integers
+// if a parse > b parse return greater
+// if a parse < b parse return less
+// if a parse === b parse return equal
