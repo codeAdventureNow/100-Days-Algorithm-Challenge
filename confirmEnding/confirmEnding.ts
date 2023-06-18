60070043bce0cebe3584a903cc35df22e6b7fa59
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-06-18 10:33:56 -0700

    confirm ending

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,12 @@
-export function confirmEnding(str: string, target: string) {
+// export function confirmEnding(str: string, target: string) {
+//   const start = str.length - target.length;
 
-}
+//   //both work
+//   //   return str.substring(start, str.length);
+//   return str.substring(start) === target;
+// }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding('Abstraction', 'action'));
+console.log(confirmEnding('Open sesame', 'pen'));
+
+//The substring() method returns the part of the string from the start index up to and excluding the end index, or to the end of the string if no end index is supplied.
