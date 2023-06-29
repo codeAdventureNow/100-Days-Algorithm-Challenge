d130ca39fa21061de37d21543b1dab087337e0b4
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-06-29 09:20:19 -0700

    enclose in brackets

--- encloseInBrackets/encloseInBrackets.ts
+++ encloseInBrackets/encloseInBrackets.ts
@@ -1,5 +1,21 @@
-export function encloseInBrackets(inputString: string): string {
+// export function encloseInBrackets(inputString: string): string {
+//   //   console.log(inputString.split(''));
 
+//   const stringArray = inputString.split('');
+
+//   stringArray.push(')');
+//   stringArray.unshift('(');
+
+//   return stringArray.join('');
+// }
+
+//Dylan solution
+export function encloseInBrackets(inputString: string): string {
+  return `(${inputString})`;
 }
 
-// console.log(encloseInBrackets('abacaba'));
+console.log(encloseInBrackets('abacaba'));
+
+//split the string
+//insert ( at index 0
+//add   ) at .length -1
