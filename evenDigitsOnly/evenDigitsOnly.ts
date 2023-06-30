21759baf40bf5cf4ab2b763d24bff8e2e30b5640
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-06-30 09:23:20 -0700

    even digits only

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,24 @@
+///my solution
+
+// export function evenDigitsOnly(n: number): boolean {
+//   const arrayAsString = n.toString().split('');
+//   const arrayAsNumber = [];
+
+//   arrayAsString.forEach((string) => {
+//     arrayAsNumber.push(parseInt(string));
+//   });
+
+//   const isEven = (number) => number % 2 === 0;
+
+//   return arrayAsNumber.every(isEven);
+// }
+
+//Dylan
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));
+
 export function evenDigitsOnly(n: number): boolean {
+  const digits = n.toString().split('');
 
+  return digits.every((digit) => parseInt(digit) % 2 === 0);
 }
-
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
