4d72c33d499709b5b8888ba689ecf6f661eab663
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-07-03 10:28:28 -0700

    factorializeNumber

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,27 @@
+//my solution
+
+// export function factorializeANumber(num: number): number {
+//   const numberArray: number[] = [];
+
+//   for (let i = 0; i <= num; i++) {
+//     if (i >= 1) {
+//       numberArray.push(i);
+//     }
+//   }
+
+//   return numberArray.reduce((acc, cur) => acc * cur, 1);
+// }
+
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
+
+//Dylan
+
 export function factorializeANumber(num: number): number {
+  let total = 1;
 
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 }
-
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
