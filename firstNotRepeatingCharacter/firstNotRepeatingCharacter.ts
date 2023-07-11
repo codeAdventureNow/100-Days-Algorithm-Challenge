5f89f9f8992f64c5d5fd90d49a7760ba7807bde3
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-07-11 09:00:11 -0700

    first not repearting character

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,31 @@
 export function firstNotRepeatingCharacter(s: string): string {
+  let duplicates = {};
+  const chars = s.split('');
+  let answer = '_';
+  let indexAnswer = Number.MAX_SAFE_INTEGER;
 
+  chars.forEach((element, index) => {
+    if (!duplicates.hasOwnProperty(element)) {
+      duplicates[element] = {
+        count: 1,
+        index,
+      };
+    } else {
+      duplicates[element].count++;
+      duplicates[element].index = index;
+    }
+  });
+
+  console.log(duplicates);
+  for (const key in duplicates) {
+    if (duplicates[key].count === 1 && duplicates[key].index < indexAnswer) {
+      answer = key;
+      indexAnswer = duplicates[key].index;
+    }
+  }
+
+  return answer;
 }
 
-// console.log(firstNotRepeatingCharacter('abacabad'));
+console.log(firstNotRepeatingCharacter('abacabad'));
 // console.log(firstNotRepeatingCharacter('abacabaabacaba'));
