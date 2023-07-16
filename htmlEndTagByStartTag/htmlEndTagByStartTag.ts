7fcd6e87c3b2a265b31a88bd96adf5647a986eed
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-07-16 08:21:16 -0700

    htmEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,21 @@
 export function htmlEndTagByStartTag(startTag: string): string {
+  let chars = startTag.split(' ');
+  let newChars = chars[0].split('');
 
+  newChars.splice(1, 0, '/');
+
+  if (newChars[newChars.length - 1] === '>') {
+    return newChars.join('');
+  }
+
+  newChars.push('>');
+  return newChars.join('');
 }
 
-// console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-// console.log(htmlEndTagByStartTag('<i>'));
+console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+console.log(htmlEndTagByStartTag('<i>'));
+
+//take the string and recreate it inserting / at index 1
+//  split the string into an array
+// insert /
+// then join together
