52eb8e4fa86c1c9a323dbf580e448db9a59fe72f
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-07-22 08:11:22 -0700

    isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,20 @@
 export function isTandemRepeat(inputString: string): boolean {
+  const wordLength = inputString.length;
 
+  if (wordLength % 2 === 0) {
+    const halfwayPoint = wordLength / 2;
+    const firstHalf = inputString.slice(0, halfwayPoint);
+    const secondHalf = inputString.slice(halfwayPoint, wordLength);
+
+    return firstHalf === secondHalf;
+  }
+  return false;
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'));
+console.log(isTandemRepeat('qqq'));
+console.log(isTandemRepeat('2w2ww'));
+
+//divide the inputString by true if the result is odd return false
+
+//if the both halves are === return true
