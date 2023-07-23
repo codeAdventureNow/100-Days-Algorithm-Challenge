3cfa9adf1c826ca570b311ba555d053e9a2ccada
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-07-23 09:41:45 -0700

    largest number

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,11 @@
 export function largestNumber(n: number): number {
+  let word = '9'.repeat(n);
 
+  return parseInt(word);
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(2));
+console.log(largestNumber(10));
+
+// -   concat()
+// -   parseInt()
