137c8fdb1764577ae9a6efa9a80c7ee56b8e25de
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-07-27 13:08:45 -0700

    longest digits prefix

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,17 @@
 export function longestDigitsPrefix(inputString: string): string {
+  const chars = inputString.split('');
+  let prefix = '';
 
+  for (let char of inputString) {
+    const num = parseInt(char);
+
+    if (isNaN(num)) {
+      break;
+    }
+    prefix += char;
+  }
+
+  return prefix;
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix('123aa1'));
