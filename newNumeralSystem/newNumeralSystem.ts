ef6762d18c8f509536b6b4a4a82428d9d05b5175
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-02 11:41:43 -0700

    new Numeral System

--- newNumeralSystem/newNumeralSystem.ts
+++ newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,19 @@
 export function newNumeralSystem(number: string): string[] {
+  const numerals: string[] = [];
 
+  let startCharCount = 65;
+  let endCharCount = number.charCodeAt(0);
+
+  while (startCharCount <= endCharCount) {
+    const numeral = `${String.fromCharCode(
+      startCharCount
+    )} + ${String.fromCharCode(endCharCount)}`;
+    numerals.push(numeral);
+    startCharCount++;
+    endCharCount--;
+  }
+
+  return numerals;
 }
 
-// console.log(newNumeralSystem('G'));
+console.log(newNumeralSystem('G'));
