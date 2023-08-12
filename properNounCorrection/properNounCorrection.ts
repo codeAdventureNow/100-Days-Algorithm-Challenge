e2cc8f74b7969639a9ae0d4943b280db4c9f49ff
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-12 08:04:38 -0700

    proper noun correction

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,21 @@
+//My solution
+// export function properNounCorrection(noun: string): string {
+//   const nounChars = noun.split('');
+//   const firstLetterCapitalized = nounChars[0].toUpperCase();
+//   const remainingLetters: string[] = [];
+
+//   for (let i = 1; i < nounChars.length; i++) {
+//     let lowerCase = nounChars[i].toLowerCase();
+//     remainingLetters.push(lowerCase);
+//   }
+//   return firstLetterCapitalized.concat(remainingLetters.join(''));
+// }
+
 export function properNounCorrection(noun: string): string {
+  const firstLetter = noun[0].toUpperCase();
+  const remainingLetters = noun.slice(1, noun.length).toLowerCase();
 
+  return firstLetter.concat(remainingLetters);
 }
-
-// console.log(properNounCorrection('pARiS'));
-// console.log(properNounCorrection('John'));
+console.log(properNounCorrection('pARiS'));
+console.log(properNounCorrection('John'));
