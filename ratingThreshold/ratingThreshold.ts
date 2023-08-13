44b7c099c445ce07564034142deb530064327913
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-13 08:20:48 -0700

    rating Threshold

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,21 @@
-export function ratingThreshold(threshold: number, ratings: number[][]): number[] {
+export function ratingThreshold(
+  threshold: number,
+  ratings: number[][]
+): number[] {
+  const review = [];
 
+  for (let i = 0; i < ratings.length; i++) {
+    let total = 0;
+
+    ratings[i].forEach((rating) => {
+      total += rating;
+    });
+
+    if (total / ratings[i].length < threshold) {
+      review.push(i);
+    }
+  }
+  return review;
 }
 
-// console.log(ratingThreshold(3.5, 
-//     [[3, 4],
-//     [3, 3, 3, 4],
-//     [4]]));
+console.log(ratingThreshold(3.5, [[3, 4], [3, 3, 3, 4], [4]]));
