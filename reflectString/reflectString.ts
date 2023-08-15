bbf1b6cf553246e87098f7a0311786f0216d606c
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-15 16:18:23 -0700

    reflectString

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,43 @@
 export function reflectString(inputString: string): string {
+  const chars: string[] = inputString.split('');
+  const reflectionLetters = {
+    a: 'z',
+    b: 'y',
+    c: 'x',
+    d: 'w',
+    e: 'v',
+    f: 'u',
+    g: 't',
+    h: 's',
+    i: 'r',
+    j: 'q',
+    k: 'p',
+    l: 'o',
+    m: 'n',
+    n: 'm',
+    o: 'l',
+    p: 'k',
+    q: 'j',
+    r: 'i',
+    s: 'h',
+    t: 'g',
+    u: 'f',
+    v: 'e',
+    w: 'd',
+    x: 'c',
+    y: 'b',
+    z: 'a',
+  };
 
+  let reflectedWords = '';
+
+  chars.forEach((char) => {
+    reflectedWords += reflectionLetters[char];
+  });
+
+  return reflectedWords;
 }
 
-// console.log(reflectString("name"));
+console.log(reflectString('name'));
+
+//a = z   a=   str
