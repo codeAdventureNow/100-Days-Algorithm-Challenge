5960620f541a3a0bd52c42b5fef3d4e28edc6b2c
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-20 18:35:57 -0700

    seats In Theater

--- seatsInTheater/seatsInTheater.ts
+++ seatsInTheater/seatsInTheater.ts
@@ -1,5 +1,12 @@
-export function seatsInTheater(nCols: number, nRows: number, col: number, row: number): number {
-
+export function seatsInTheater(
+  nCols: number,
+  nRows: number,
+  col: number,
+  row: number
+): number {
+  const colDif = nCols - col + 1;
+  const rowDif = nRows - row;
+  return colDif * rowDif;
 }
 
-// console.log(seatsInTheater(16, 11, 5, 3));
+console.log(seatsInTheater(16, 11, 5, 3));
